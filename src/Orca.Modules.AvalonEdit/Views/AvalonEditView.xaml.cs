9741b66a2301db4ace7589f86383181d4dddee6f
bigN13 <sivegr@yahoo.gr>
2013-10-03 07:17:33 +0300

    1st commit

---
+++ src/Orca.Modules.AvalonEdit/Views/AvalonEditView.xaml.cs
@@ -0,0 +1,74 @@
+﻿using ICSharpCode.AvalonEdit.CodeCompletion;
+using Orca.Modules.AvalonEdit.Common;
+using Orchestra.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Orca.Modules.AvalonEdit.Views
+{
+    /// <summary>
+    /// Interaction logic for AvalonEditView.xaml
+    /// </summary>
+    public partial class AvalonEditView :  DocumentView
+    {
+        public AvalonEditView()
+        {
+            InitializeComponent();
+            // in the constructor:
+            textEditor.TextArea.TextEntering += textEditor_TextArea_TextEntering;
+            textEditor.TextArea.TextEntered += textEditor_TextArea_TextEntered;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Heloo you");
+        }
+
+        CompletionWindow completionWindow;
+
+        void textEditor_TextArea_TextEntered(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text == ".")
+            {
+                // Open code completion after the user has pressed dot:
+                completionWindow = new CompletionWindow(textEditor.TextArea);
+                IList<ICompletionData> data = completionWindow.CompletionList.CompletionData;
+                data.Add(new MyCompletionData("Item1"));
+                data.Add(new MyCompletionData("Item2"));
+                data.Add(new MyCompletionData("Item3"));
+                completionWindow.Show();
+                completionWindow.Closed += delegate
+                {
+                    completionWindow = null;
+                };
+            }
+        }
+
+        void textEditor_TextArea_TextEntering(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text.Length > 0 && completionWindow != null)
+            {
+                if (!char.IsLetterOrDigit(e.Text[0]))
+                {
+                    // Whenever a non-letter is typed while the completion window is open,
+                    // insert the currently selected element.
+                    completionWindow.CompletionList.RequestInsertion(e);
+                }
+            }
+            // Do not set e.Handled=true.
+            // We still want to insert the character that was typed.
+        }
+    }
+}
---
+++ src/Orca/App.xaml.cs
@@ -0,0 +1,100 @@
+﻿using Catel.IoC;
+using Catel.MVVM;
+using Catel.Windows;
+using Orchestra;
+using Orchestra.Services;
+using Orchestra.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace Orca
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        #region Methods
+        /// <summary>
+        /// Raises the <see cref="E:System.Windows.Application.Startup"/> event.
+        /// </summary>
+        /// <param name="e">
+        /// A <see cref="T:System.Windows.StartupEventArgs"/> that contains the event data.
+        /// </param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            SetCurrentCulture();
+
+            // Example of best performance options for Catel (but at the cost of validation features)
+            Catel.Windows.Controls.UserControl.DefaultCreateWarningAndErrorValidatorForViewModelValue = false;
+            Catel.Windows.Controls.UserControl.DefaultSkipSearchingForInfoBarMessageControlValue = true;
+            Catel.Data.ModelBase.SuspendValidationForAllModels = true;
+
+            var serviceLocator = ServiceLocator.Default;
+            Catel.Environment.RegisterDefaultViewModelServices();
+
+            var viewLocator = serviceLocator.ResolveType<IViewLocator>();
+            viewLocator.Register(typeof(SplashScreenViewModel), typeof(Orchestra.Views.SplashScreen));
+
+            var viewModelLocator = serviceLocator.ResolveType<IViewModelLocator>();
+            viewModelLocator.Register(typeof(SplashScreen), typeof(SplashScreenViewModel));
+
+            var bootstrapper = new OrchestraBootstrapper();
+
+            StyleHelper.CreateStyleForwardersForDefaultStyles(Current.Resources.MergedDictionaries[1]);
+
+            bootstrapper.CreatedShell += (sender, e2) =>
+            {
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    // Configure shell when it's created
+                    var configurationService = ServiceLocator.Default.ResolveType<IConfigurationService>();
+                    ConfigureShell(configurationService);
+
+                    // Disable debugging window
+                    var orchestraService = ServiceLocator.Default.ResolveType<IOrchestraService>();
+                    orchestraService.ShowDebuggingWindow = false;
+                });
+            };
+
+            bootstrapper.RunWithSplashScreen<SplashScreenViewModel>();
+
+            base.OnStartup(e);
+        }
+
+        /// <summary>
+        /// Exemple on how to configure the shell.
+        /// </summary>
+        /// <param name="configurationService">
+        /// The configuration service.
+        /// </param>
+        private static void ConfigureShell(IConfigurationService configurationService)
+        {
+            // Override the configarable items in the Shell by localized items.
+            //configurationService.Configuration.HelpTabText = Orchestra.Resources.Resources.HelpRibbonTabText;
+            //configurationService.Configuration.HelpGroupText = Orchestra.Resources.Resources.HelpGroupText;
+            //configurationService.Configuration.HelpButtonText = Orchestra.Resources.Resources.HelpButtonText;
+        }
+
+        private static void SetCurrentCulture()
+        {
+            ////Example culture, for testing purposes.
+            // var culture = new CultureInfo("de-DE");
+
+            // Thread.CurrentThread.CurrentCulture = culture;
+            // Thread.CurrentThread.CurrentUICulture = culture;
+
+            // Changes the Default WPF Culture (en-US), otherwise it will be used, instead of the system settings.
+            FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+        }
+        #endregion
+    }
+}
