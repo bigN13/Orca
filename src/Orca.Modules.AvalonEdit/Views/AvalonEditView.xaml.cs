7393b5512ceccc6b309a5855c6e58527295a3eec
bigN13 <sivegr@yahoo.gr>
2013-10-06 09:30:45 +0300

    Added Copy, Paset, Format, Folding features

--- src/Orca.Modules.AvalonEdit/Views/AvalonEditView.xaml.cs
+++ src/Orca.Modules.AvalonEdit/Views/AvalonEditView.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using ICSharpCode.AvalonEdit.CodeCompletion;
+using ICSharpCode.AvalonEdit.Folding;
+using ICSharpCode.AvalonEdit.Highlighting;
+using Microsoft.Win32;
 using Orca.Modules.AvalonEdit.Common;
 using Orchestra.Views;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +19,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Xml;
 
 namespace Orca.Modules.AvalonEdit.Views
 {
@@ -26,10 +32,90 @@ namespace Orca.Modules.AvalonEdit.Views
         public AvalonEditView()
         {
             InitializeComponent();
+
+            // Load our custom highlighting definition
+            //IHighlightingDefinition customHighlighting;
+            //using (Stream s = typeof(AvalonEditView).Assembly.GetManifestResourceStream("AvalonEdit.Sample.CustomHighlighting.xshd"))
+            //{
+            //    if (s == null)
+            //        throw new InvalidOperationException("Could not find embedded resource");
+            //    using (XmlReader reader = new XmlTextReader(s))
+            //    {
+            //        customHighlighting = ICSharpCode.AvalonEdit.Highlighting.Xshd.
+            //            HighlightingLoader.Load(reader, HighlightingManager.Instance);
+            //    }
+            //}
+            //// and register it in the HighlightingManager
+            //HighlightingManager.Instance.RegisterHighlighting("Custom Highlighting", new string[] { ".cool" }, customHighlighting);
+
+
+
+            propertyGridComboBox.SelectedIndex = 2;
+
             // in the constructor:
             textEditor.TextArea.TextEntering += textEditor_TextArea_TextEntering;
             textEditor.TextArea.TextEntered += textEditor_TextArea_TextEntered;
+
+            //DispatcherTimer foldingUpdateTimer = new DispatcherTimer();
+            //foldingUpdateTimer.Interval = TimeSpan.FromSeconds(2);
+            //foldingUpdateTimer.Tick += foldingUpdateTimer_Tick;
+            //foldingUpdateTimer.Start();
         }
+
+        string currentFileName;
+
+        #region Commands
+        void openFileClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.CheckFileExists = true;
+            if (dlg.ShowDialog() ?? false)
+            {
+                currentFileName = dlg.FileName;
+                textEditor.Load(currentFileName);
+                textEditor.SyntaxHighlighting = HighlightingManager.Instance.GetDefinitionByExtension(System.IO.Path.GetExtension(currentFileName));
+            }
+        }
+
+        void saveFileClick(object sender, EventArgs e)
+        {
+            if (currentFileName == null)
+            {
+                SaveFileDialog dlg = new SaveFileDialog();
+                dlg.DefaultExt = ".txt";
+                if (dlg.ShowDialog() ?? false)
+                {
+                    currentFileName = dlg.FileName;
+                }
+                else
+                {
+                    return;
+                }
+            }
+            textEditor.Save(currentFileName);
+        }
+
+        void propertyGridComboBoxSelectionChanged(object sender, RoutedEventArgs e)
+        {
+            if (propertyGrid == null)
+                return;
+            switch (propertyGridComboBox.SelectedIndex)
+            {
+                case 0:
+                    propertyGrid.SelectedObject = textEditor;
+                    break;
+                case 1:
+                    propertyGrid.SelectedObject = textEditor.TextArea;
+                    break;
+                case 2:
+                    propertyGrid.SelectedObject = textEditor.Options;
+                    break;
+            }
+        }
+
+        #endregion
+
+        //CompletionWindow completionWindow;
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -70,5 +156,62 @@ namespace Orca.Modules.AvalonEdit.Views
             // Do not set e.Handled=true.
             // We still want to insert the character that was typed.
         }
+
+        #region Folding
+        FoldingManager foldingManager;
+        AbstractFoldingStrategy foldingStrategy;
+
+        void HighlightingComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (textEditor.SyntaxHighlighting == null)
+            {
+                foldingStrategy = null;
+            }
+            else
+            {
+                switch (textEditor.SyntaxHighlighting.Name)
+                {
+                    case "XML":
+                        foldingStrategy = new XmlFoldingStrategy();
+                        textEditor.TextArea.IndentationStrategy = new ICSharpCode.AvalonEdit.Indentation.DefaultIndentationStrategy();
+                        break;
+                    case "C#":
+                    case "C++":
+                    case "PHP":
+                    case "Java":
+                        textEditor.TextArea.IndentationStrategy = new ICSharpCode.AvalonEdit.Indentation.CSharp.CSharpIndentationStrategy(textEditor.Options);
+                        foldingStrategy = new BraceFoldingStrategy();
+                        break;
+                    default:
+                        textEditor.TextArea.IndentationStrategy = new ICSharpCode.AvalonEdit.Indentation.DefaultIndentationStrategy();
+                        foldingStrategy = null;
+                        break;
+                }
+            }
+            if (foldingStrategy != null)
+            {
+                if (foldingManager == null)
+                    foldingManager = FoldingManager.Install(textEditor.TextArea);
+                foldingStrategy.UpdateFoldings(foldingManager, textEditor.Document);
+            }
+            else
+            {
+                if (foldingManager != null)
+                {
+                    FoldingManager.Uninstall(foldingManager);
+                    foldingManager = null;
+                }
+            }
+        }
+
+        void foldingUpdateTimer_Tick(object sender, EventArgs e)
+        {
+            if (foldingStrategy != null)
+            {
+                foldingStrategy.UpdateFoldings(foldingManager, textEditor.Document);
+            }
+        }
+        #endregion
+
     }
 }
